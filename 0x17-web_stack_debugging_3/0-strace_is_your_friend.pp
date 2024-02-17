52dde520c04b065655c02657c4963975cbc4567f
Nkiru Ezefosie <nkiru.happiness@gmail.com>
2024-02-16 23:33:47 -0800

    Web stack debugging #3

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Fixed bad "phpp" extensions to "php in "wp_settings.php"
+
+exec {'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
